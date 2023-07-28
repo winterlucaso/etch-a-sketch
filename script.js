526fee1b5adba059c60a363fae6313fbd8513dca
winterlucaso <58354096+winterlucaso@users.noreply.github.com>
2023-07-28 13:52:11 -0500

    create rough css outline and flexbox grid for main etch-a-sketch display

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 
+// Initialize Grid
+function initializeGrid(gridSize) {
+    for (let x = 0; x < gridSize; x++) {
+        const gridRow = document.createElement('div');
+        gridRow.classList.add("gridRow");
+        grid.appendChild(gridRow);
+        for (let y = 0; y < gridSize; y++) {
+            const gridSquare = document.createElement('div');
+            gridSquare.classList.add("gridSquare");
+            gridRow.appendChild(gridSquare);
+        }
+    }
+
+}
+
+// UI
+const grid = document.querySelector('.grid')
+const gridSquare = document.querySelectorAll('.gridSquare')
+
+// Script
+let gridSize = 8;
+initializeGrid(gridSize);
