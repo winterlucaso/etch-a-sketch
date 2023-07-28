ff3247d0a3be0105daae97a56935326becd9ea1c
winterlucaso <58354096+winterlucaso@users.noreply.github.com>
2023-07-28 15:33:40 -0500

    add listener and return square on click

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 
-// Initialize Grid
+// Initialize Grid and create squares
 function initializeGrid(gridSize) {
-    for (let x = 0; x < gridSize; x++) {
-        const gridRow = document.createElement('div');
-        gridRow.classList.add("gridRow");
-        grid.appendChild(gridRow);
-        for (let y = 0; y < gridSize; y++) {
-            const gridSquare = document.createElement('div');
-            gridSquare.classList.add("gridSquare");
-            gridRow.appendChild(gridSquare);
-        }
+    gridContainer.style.gridTemplateColumns = "repeat(" + `${gridSize}` + ", 1fr)";
+    for (let x = 0; x < gridSize**2; x++) {
+        const gridSquare = document.createElement('div');
+        gridSquare.classList.add("gridSquare");
+        gridSquare.setAttribute('id', "sqr" + `${x}`);
+        gridSquare.addEventListener('click', () => {
+            console.log(gridSquare);
+            // changeColor(gridSquare);
+        });
+        gridContainer.appendChild(gridSquare);
     }
+}
 
+function changeColor(e) {
+    e.style.color = "black";
 }
 
 // UI
-const grid = document.querySelector('.grid')
-const gridSquare = document.querySelectorAll('.gridSquare')
+const gridContainer = document.querySelector('.grid-container')
+const btn1 = document.getElementById('btn1')
+const btn2 = document.getElementById('btn2')
+const btn3 = document.getElementById('btn3')
+const btn4 = document.getElementById('btn4')
+
+
 
 // Script
 let gridSize = 8;
